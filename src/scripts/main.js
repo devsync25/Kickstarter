9ba1dfa90ebc15d8372624ea97415f80d4e76fa9
Elvira Petrenko <rukola2008_r@users.noreply.github.com>
2026-09-06 16:45:59 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,52 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.getElementById('menu-toggle');
+  const menuClose = document.getElementById('menu-close');
+  const mobileMenu = document.getElementById('mobile-menu');
+  const body = document.body;
+
+  const menuLinks = mobileMenu ? mobileMenu.querySelectorAll('a') : [];
+
+  if (!menuToggle || !mobileMenu) {
+    console.warn("DOM!");
+      return;
+  }
+
+
+  const openMenu = () => {
+    mobileMenu.classList.add('is-open');
+    mobileMenu.setAttribute('aria-hidden', 'false');
+    menuToggle.setAttribute('aria-expanded', 'true');
+    body.classList.add('lock');
+  };
+
+  const closeMenu = () => {
+    mobileMenu.classList.remove('is-open');
+    mobileMenu.setAttribute('aria-hidden', 'true');
+    menuToggle.setAttribute('aria-expanded', 'false');
+    body.classList.remove('lock');
+  };
+
+  menuToggle.addEventListener('click', (e) => {
+    e.stopPropagation();
+
+    if (mobileMenu.classList.contains('is-open')) {
+      closeMenu();
+    } else {
+      openMenu();
+    }
+  });
+
+  if (menuClose) {
+    menuClose.addEventListener('click', closeMenu);
+  }
+
+  menuLinks.forEach(link => {
+    link.addEventListener('click', closeMenu);
+  });
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && mobileMenu.classList.contains('is-open')) {
+      closeMenu();
+    }
+  });
+});
